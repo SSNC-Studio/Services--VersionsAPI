33d996a6a48bcb57525b46b0d6bea950d76825b1
SSNC-Studio <ssnc.studios@gmail.com>
2026-05-20 16:53:14 +0000

    Version 1.0.1.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const { version } = require("react");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,7 +10,25 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 
+const CURRENT_VERSION_JSON = {
+    version: "1.0.1.1",
+    author: "[FOUNDER] SPHLAC",
+    contributors: "(Nobody)",
+    compileDate: "20-05-2026 (dd/mm/yyyy)",
+    allowPreviousVersions: false,
+    DEPRECATED: false,
+};
+const CURRENT_VERSION = "1.0.1.1";
+
 app.get("/api/script/GLOBAL-LOADING-SCREEN", (req, res) => {
+    res.json(CURRENT_VERSION_JSON);
+});
+
+app.get(`/api/script/GLOBAL-LOADING-SCREEN/${CURRENT_VERSION}`, (req, res) => {
+    res.json(CURRENT_VERSION_JSON);
+});
+
+app.get("/api/script/GLOBAL-LOADING-SCREEN/1.0.1", (req, res) => {
     res.json({
         version: "1.0.1",
         author: "[FOUNDER] SPHLAC",
@@ -17,8 +36,8 @@ app.get("/api/script/GLOBAL-LOADING-SCREEN", (req, res) => {
         compileDate: "17-05-2026 (dd/mm/yyyy)",
         allowPreviousVersions: false,
         DEPRECATED: false,
-    });
-});
+    })
+})
 
 app.get("/api/script/GLOBAL-LOADING-SCREEN/1.0.0", (req, res) => {
     res.json({
